857e886ddaaaecb5265abb10e9e35bb89ea02c74
Elbert Tse <etse168@gmail.com>
2020-09-29 21:19:35 -0400

    First commit

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+// home is where stuff starts
+// center takes a child
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello World'),
+      ),
+    ),
+  );
+}
