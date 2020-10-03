d69303fd6ba35a680ef4077ff0d884566542a359
Elbert <etse168@gmail.com>
2020-10-03 12:12:54 -0400

    finished quizzler

---
+++ quizzler-flutter/lib/question.dart
@@ -0,0 +1,12 @@
+class Question{
+  String text;
+  bool answer;
+
+  Question(this.text, this.answer); // is equivalent to 
+  /*
+  Question(String q, bool a){
+    text = q;
+    answer = a;
+  }
+  */
+}
